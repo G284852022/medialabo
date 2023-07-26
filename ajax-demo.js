377f8acae1a844d50beb371dc1128ade8a797764
G284852022 <228485@st.takushoku-u.ac.jp>
2023-07-27 07:52:39 +0900

    sinki

--- ajax-demo.js
+++ ajax-demo.js
@@ -4,39 +4,39 @@ b.addEventListener('click', sendRequest);
 
 // 通信を開始する処理
 function sendRequest() {
-	// URL を設定
-	let url = 'https://www.nishita-lab.org/web-contents/jsons/test.json';
+    // URL を設定
+    let url = 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js';
 
-	// 通信開始
-	axios.get(url)
-		.then(showResult)
-		.catch(showError)
-		.then(finish);
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 }
 
 // 通信が成功した時の処理
 function showResult(resp) {
-	// サーバから送られてきたデータを出力
-	let data = resp.data;
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
 
-	// data が文字列型なら，オブジェクトに変換する
-	if (typeof data === 'string') {
-		data = JSON.parse(data);
-	}
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
 
-	// data をコンソールに出力
-	console.log(data);
+    // data をコンソールに出力
+    console.log(data);
 
-	// data.x を出力
-	console.log(data.x);
+    // data.x を出力
+    console.log(data.x);
 }
 
 // 通信エラーが発生した時の処理
 function showError(err) {
-	console.log(err);
-}	
+    console.log(err);
+}
 
 // 通信の最後にいつも実行する処理
 function finish() {
-	console.log('Ajax 通信が終わりました');
+    console.log('Ajax 通信が終わりました');
 }
