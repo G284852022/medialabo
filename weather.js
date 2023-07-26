db8b4eaeb180608c5d047008d2782c8aece1a963
G284852022 <228485@st.takushoku-u.ac.jp>
2023-07-26 16:27:46 +0900

    sinnki

--- weather.js
+++ weather.js
@@ -46,4 +46,45 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
+
+// 通信を開始する処理
+function sendRequest() {
+    // URL を設定
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/{id}.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
